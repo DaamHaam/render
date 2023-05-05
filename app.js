57a1b019656c5cee95e2757536bba61ab39a3b28
DaamHaam <63154077+DaamHaam@users.noreply.github.com>
2023-05-05 08:55:25 +0200

    ancienne version remise

--- app.js
+++ app.js
@@ -5,32 +5,30 @@ const app = express();
 const ip = require('ip');
 const port = 3000; // Remplacez 3000 par le numéro de port que vous utilisez pour votre serveur Node.js
 
-console.log("IP : " + ip.address() + ":3000");
+console.log("IP : " + ip.address() + ":" + port);
 
 app.use(express.static('public'));
 
 app.get('/P1TD', function(req, res) {
-  res.sendFile(path.join(__dirname,  'P1TD', 'index.html'));
+  res.sendFile(path.join(__dirname, 'P1TD', 'index.html'));
 });
 
 app.get('/script.js', function(req, res) {
-  res.sendFile(path.join(__dirname, 'public',  'P1TD', 'js', 'script.js'));
+  res.sendFile(path.join(__dirname, 'public', 'P1TD', 'js', 'script.js'));
 });
 
 app.get('/manifest.json', function(req, res) {
-  res.sendFile(path.join(__dirname, 'public',  'P1TD', 'manifest.json'));
+  res.sendFile(path.join(__dirname, 'public', 'P1TD', 'manifest.json'));
 });
 
 app.get('/sw.js', function(req, res) {
-  res.sendFile(path.join(__dirname, 'public',  'P1TD', 'sw.js'));
+  res.sendFile(path.join(__dirname, 'public', 'P1TD', 'sw.js'));
 });
-
 
 //exos patients
 app.get('/P2EP', function(req, res) {
   res.sendFile(path.join(__dirname, 'public', 'P2EP', 'index.html'));
 });
-
 
 app._router.stack.forEach(function(route) {
   if (route.route && route.route.path !== '/') {
@@ -38,8 +36,6 @@ app._router.stack.forEach(function(route) {
   }
 });
 
-
-
 app.listen(port, function() {
-  console.log('Server started on port ${port}');
+  console.log('Server started on port ' + port);
 });
