fca439518c382787b729f552b64924ac35897ffa
DaamHaam <63154077+DaamHaam@users.noreply.github.com>
2023-05-09 15:41:11 +0200

    Ajout d'Express, réorganisation des fichiers et ajout de la page d'accueil

--- app.js
+++ app.js
@@ -1,8 +1,18 @@
-const http = require('http');
+const express = require('express');
+const app = express();
+const path = require('path');
 
-function handleRequest(req, res) {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello World du serveur !\n');
-}
+// Pour servir les fichiers statiques dans le dossier 'public'
+app.use(express.static(path.join(__dirname, 'public')));
 
-module.exports = handleRequest;
+// Route pour la page d'accueil (index.html)
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+// Ajoutez ici des routes pour d'autres pages HTML, si nécessaire
+// app.get('/autre-page', (req, res) => {
+//   res.sendFile(path.join(__dirname, 'public', 'autre-page.html'));
+// });
+
+module.exports = app;
