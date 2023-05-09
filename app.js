8665d226db43869f5a721b9a878a54181e082510
DaamHaam <63154077+DaamHaam@users.noreply.github.com>
2023-05-09 15:28:52 +0200

    serveur de base hello

--- app.js
+++ app.js
@@ -1,41 +1,8 @@
-const express = require('express');
-const path = require('path');
-const app = express();
+const http = require('http');
 
-const ip = require('ip');
-const port = 3000; // Remplacez 3000 par le numéro de port que vous utilisez pour votre serveur Node.js
+function handleRequest(req, res) {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Hello World du serveur !\n');
+}
 
-console.log("IP : " + ip.address() + ":" + port);
-
-app.use(express.static('public'));
-
-app.get('/P1TD', function(req, res) {
-  res.sendFile(path.join(__dirname, 'P1TD', 'index.html'));
-});
-
-app.get('/script.js', function(req, res) {
-  res.sendFile(path.join(__dirname, 'public', 'P1TD', 'js', 'script.js'));
-});
-
-app.get('/manifest.json', function(req, res) {
-  res.sendFile(path.join(__dirname, 'public', 'P1TD', 'manifest.json'));
-});
-
-app.get('/sw.js', function(req, res) {
-  res.sendFile(path.join(__dirname, 'public', 'P1TD', 'sw.js'));
-});
-
-//exos patients
-app.get('/P2EP', function(req, res) {
-  res.sendFile(path.join(__dirname, 'public', 'P2EP', 'index.html'));
-});
-
-app._router.stack.forEach(function(route) {
-  if (route.route && route.route.path !== '/') {
-    console.log(`http://${ip.address()}:${port}${route.route.path}`);
-  }
-});
-
-app.listen(port, function() {
-  console.log('Server started on port ' + port);
-});
+module.exports = handleRequest;
